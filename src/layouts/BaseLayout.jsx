00eacac271a31d53afce1d19545a84b4aee66571
abrahamuchos <j.abraham29@gmail.com>
2025-05-03 19:19:11 -0400

    Add navigation

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from "react-router";
+import Navigation from "../components/Navigation.jsx";
 
 export default function BaseLayout() {
   return (
     <>
+      <Navigation/>
       <h1>Base Layout</h1>
       <main>
         <Outlet/>
