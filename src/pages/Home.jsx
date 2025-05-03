9df0e4d26a5c0db0ad5435756067f5e6f745b3ff
abrahamuchos <j.abraham29@gmail.com>
2025-05-03 15:58:14 -0400

    Add tailwind library, fonts and soome styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,10 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline bg-blue-300">
         Home
       </h1>
+      <p className="heading-1 bg-black">Welcome to my page</p>
+      <p className="text-3xl font-Instrument">Welcome to my page</p>
+      <p className="text-3xl font-GameOver">Welcome to my page</p>
+      <p className="text-3xl font-Geist">Welcome to my page</p>
     </div>
   );
 }
