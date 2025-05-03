0fe9c44db87d69493b31a9047ebaf113fb50381e
abrahamuchos <j.abraham29@gmail.com>
2025-05-03 19:39:48 -0400

    Add footer and define padding

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Outlet } from "react-router";
 import Navigation from "../components/Navigation.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function BaseLayout() {
   return (
-    <>
+    <div className='relative flex flex-col min-h-screen'>
       <Navigation/>
-      <h1>Base Layout</h1>
-      <main>
+      <main className='flex-grow px-2 lg:px-12'>
         <Outlet/>
       </main>
-    </>
+      <Footer/>
+    </div>
   );
 }
