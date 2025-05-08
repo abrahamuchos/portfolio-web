748ca0ba6fbed4068c7f7786d790a1745b5f0451
abrahamuchos <j.abraham29@gmail.com>
2025-05-08 17:34:25 -0400

    Add products list

---
+++ src/assets/data/posts.js
@@ -0,0 +1,67 @@
+export const posts = [
+  {
+    // title: "My first post ",
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim eros tincidunt nulla tincidunt laoreet.  ",
+    subtitle: "This is my first post",
+    description: "This is my first post",
+    content: "This is my first post",
+    slug: "my-first-post",
+    date: "2021-01-01",
+    header: '/src/assets/images/posts/image-post.png'
+  },
+  {
+    title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim eros tincidunt nulla tincidunt laoreet. Sed dignissim eros tincidunt nulla tincidunt laoreet.",
+    // subtitle: "This is my second post",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim eros tincidunt nulla tincidunt laoreet. Sed dignissim eros tincidunt nulla tincidunt laoreet.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse volutpat ac metus sed porttitor. Sed eu maximus ligula, ac molestie augue.",
+    content: "This is my second post",
+    slug: "my-second-post",
+    date: "2021-01-01",
+    header: '/src/assets/images/posts/image-post.png'
+  },
+  {
+    title: "My third post",
+    subtitle: "This is my third post",
+    description: "This is my third post",
+    content: "This is my third post",
+    slug: "my-third-post",
+    date: "2021-01-01",
+    header: '/src/assets/images/posts/image-post.png'
+  },
+  {
+    title: "My fourth post",
+    subtitle: "This is my fourth post",
+    description: "This is my fourth post",
+    content: "This is my fourth post",
+    slug: "my-fourth-post",
+    date: "2021-01-01",
+    header: '/src/assets/images/posts/image-post.png'
+  },
+  {
+    title: "My fifth post",
+    subtitle: "This is my fifth post",
+    description: "This is my fifth post",
+    content: "This is my fifth post",
+    slug: "my-fifth-post",
+    date: "2021-01-01",
+    header: '/src/assets/images/posts/image-post.png'
+  },
+  {
+    title: "My sixth post",
+    subtitle: "This is my sixth post",
+    description: "This is my sixth post",
+    content: "This is my sixth post",
+    slug: "my-sixth-post",
+    date: "2021-01-01",
+    header: '/src/assets/images/posts/image-post.png'
+  },
+  {
+    title: "My seventh post",
+    subtitle: "This is my seventh post",
+    description: "This is my seventh post",
+    content: "This is my seventh post",
+    slug: "my-seventh-post",
+    date: "2021-01-01",
+    header: '/src/assets/images/posts/image-post.png'
+  },
+];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import SeparatorCommand from "../components/SeparatorCommand.jsx";
+import { posts } from "../assets/data/posts.js";
+import Card from "../components/Card.jsx";
+import ContactCard from "../components/ContactCard.jsx";
 
 export default function Projects() {
+
+  const firstPost = posts[0];
+  const remainingPosts = posts.slice(1);
+
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <>
+      <div className='relative mb-10 lg:mb-12'>
+        <SeparatorCommand>
+          php artisan project:list
+        </SeparatorCommand>
+      </div>
+
+      <section className='mb-10'>
+        <div className='w-full mb-5'>
+          <Card
+            variant='primary'
+            title={firstPost.title}
+            subtitle={firstPost.subtitle}
+            description={firstPost.description}
+            image={firstPost.header}
+          />
+        </div>
+        <div className="grid grid-cols-1 gap-5 justify-items-center md:grid-cols-2 lg:grid-cols-3">
+          {remainingPosts.map((post, index) => (
+            <Card
+              key={index}
+              variant='secondary'
+              title={post.title}
+              subtitle={post.subtitle}
+              description={post.description}
+              image={post.header}
+            />
+          ))}
+        </div>
+
+      </section>
+
+      <div className='relative mb-10 lg:mb-12'>
+        <SeparatorCommand>
+          php artisan contact:list
+        </SeparatorCommand>
+      </div>
+
+      <section className='w-full mt-16 mb-20'>
+        <ContactCard/>
+      </section>
+
+    </>
   );
 }
